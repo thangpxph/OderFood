d63e4f45ed082dac87c16c7443fd8855e1947198
phamthang <you@example.com>
2020-10-16 17:20:52 +0700

    CategoryManager thangpx

---
+++ app/src/main/java/com/example/oderfood/model/Category.java
@@ -0,0 +1,26 @@
+package com.example.oderfood.model;
+
+public class Category {
+    private int image;
+    private String name;
+
+    public Category(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
