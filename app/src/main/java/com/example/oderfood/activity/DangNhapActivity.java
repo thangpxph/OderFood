c7839e83d076342b436ce889d2d9c5036a4e3662
Nguyễn Đạo <daokaka15051998@gmail.com>
2020-11-01 20:30:47 +0700

    test

---
+++ app/src/main/java/com/example/oderfood/activity/DangNhapActivity.java
@@ -0,0 +1,16 @@
+package com.example.oderfood.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oderfood.R;
+
+public class DangNhapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
