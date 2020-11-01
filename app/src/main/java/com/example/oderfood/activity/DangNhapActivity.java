c117119ca5708dab88dfc8035a2ea82dac3717fe
Nguyễn Đạo <daokaka15051998@gmail.com>
2020-11-01 20:40:27 +0700

    a

--- app/src/main/java/com/example/oderfood/activity/DangNhapActivity.java
+++ app/src/main/java/com/example/oderfood/activity/DangNhapActivity.java
@@ -12,5 +12,7 @@ public class DangNhapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+
+        //aananan
     }
 }
