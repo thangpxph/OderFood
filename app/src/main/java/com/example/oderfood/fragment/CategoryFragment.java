6fe42e3cb01e435741b37d9a1d16996092313919
haidm <haidm@newwave.vn>
2020-11-02 16:37:16 +0700

    custom bottom navigation

---
+++ app/src/main/java/com/example/oderfood/fragment/CategoryFragment.java
@@ -0,0 +1,22 @@
+package com.example.oderfood.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.oderfood.R;
+
+public class CategoryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/oderfood/fragment/HistoryFragment.java
@@ -0,0 +1,23 @@
+package com.example.oderfood.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.oderfood.R;
+
+public class HistoryFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        return view;
+    }
+}
