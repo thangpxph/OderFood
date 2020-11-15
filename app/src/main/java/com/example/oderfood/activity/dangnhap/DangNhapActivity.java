1a8df4181e609ba6f51635efde72df70c86d8b03
Nguyễn Đạo <daokaka15051998@gmail.com>
2020-11-15 20:09:19 +0700

    abcd

--- app/src/main/java/com/example/oderfood/activity/dangnhap/DangNhapActivity.java
+++ app/src/main/java/com/example/oderfood/activity/dangnhap/DangNhapActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,12 @@ import com.example.oderfood.activity.dangky.DangKyActivity;
 import com.example.oderfood.activity.gui.GuiActivity;
 import com.example.oderfood.model.User;
 import com.example.oderfood.sql.UserSql;
+import com.google.android.material.snackbar.Snackbar;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DangNhapActivity extends AppCompatActivity {
 
@@ -44,21 +51,62 @@ public class DangNhapActivity extends AppCompatActivity {
         startActivity(new Intent(DangNhapActivity.this, classz));
     }
 
-    public void checkLogin(View view) {
-        Log.i("info", "check login");
-        userSql = new UserSql(this);
-        String email = edtEmail.getText().toString();
-        String password = edtPassWord.getText().toString();
-        Log.i("info",email+ " : " + password);
-        User result = userSql.getUser(email);
-        Log.i("info",result.toString());
-        if (null != result && password.equals(result.getPassword())) {
-            Toast.makeText(getBaseContext(), "Đăng Nhập thành công", Toast.LENGTH_SHORT).show();
-            Log.i("info","Login thanh cong");
-            transferView(MainActivity.class);
-        } else {
-            Toast.makeText(getBaseContext(), "Đăng nhập thất bại,vui lòng kiểm tra lại ", Toast.LENGTH_SHORT).show();
-            Log.i("info","Login that bai");
+//    public void checkLogin(View view) {
+//        Log.i("info", "check login");
+//        userSql = new UserSql(this);
+//        String email = edtEmail.getText().toString();
+//        String password = edtPassWord.getText().toString();
+//        Log.i("info",email+ " : " + password);
+//        User result = userSql.getUser(email);
+//        Log.i("info",result.toString());
+//        if (null != result && password.equals(result.getPassword())) {
+//            Toast.makeText(getBaseContext(), "Đăng Nhập thành công", Toast.LENGTH_SHORT).show();
+//            Log.i("info","Login thanh cong");
+//            transferView(MainActivity.class);
+//        } else {
+//            Toast.makeText(getBaseContext(), "Đăng nhập thất bại,vui lòng kiểm tra lại ", Toast.LENGTH_SHORT).show();
+//            Log.i("info","Login that bai");
+//        }
+//    }
+
+    public  void checkLogin(View view){
+        Ion.with(this)
+                .load("POST", "https://4e60c873-9ab1-478a-8784-7ae5416cb406.mock.pstmn.io/login")
+                .setBodyParameter("username", edtEmail.getText().toString())
+                .setBodyParameter("password", edtPassWord.getText().toString())
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        if (e == null) {
+                            JSONObject o = silentStringToJson(result);
+                            String error = o.optString("error");
+                            String token = o.optString("token");
+                            if (TextUtils.isEmpty(error)) {
+                                //TODO: Save token
+                                transferView(MainActivity.class);
+                            } else {
+                                //Show error
+                                Snackbar.make(edtEmail,
+                                        error,
+                                        Snackbar.LENGTH_SHORT)
+                                        .show();
+                            }
+                        } else {
+                            Snackbar.make(edtEmail,
+                                    "Đăng nhập thất bại,vui lòng kiểm tra lại",
+                                    Snackbar.LENGTH_SHORT)
+                                    .show();
+                        }
+                        // Todo something
+                    }
+                });
+    }
+    public JSONObject silentStringToJson(String result) {
+        try {
+            return new JSONObject(result);
+        } catch (JSONException e) {
+            return null;
         }
     }
 }
