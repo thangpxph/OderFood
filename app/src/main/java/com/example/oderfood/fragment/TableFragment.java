fe4ae221522d6a4e0e27ffc164b4f3e418f21dab
Minh Hai <gitkudo0504@gmail.com>
2020-11-18 13:37:15 +0700

    no message

--- app/src/main/java/com/example/oderfood/fragment/TableFragment.java
+++ app/src/main/java/com/example/oderfood/fragment/TableFragment.java
@@ -3,6 +3,7 @@ package com.example.oderfood.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +13,14 @@ import com.example.oderfood.R;
 
 
 public class TableFragment extends Fragment {
-
+private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_table, container, false);
+        recyclerView = view.findViewById(R.id.tableList);
         return view;
     }
 }
