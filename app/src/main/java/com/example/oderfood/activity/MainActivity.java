9f89d9a56dc32de427d612da795f4a2aaa920dd5
daokaka <daokaka15051998@gmail.com>
2020-11-19 21:25:31 +0700

    Merge remote-tracking branch 'origin/feature/login_template' into feature/login_template
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable-hdpi/chili.png
    #	app/src/main/res/drawable-mdpi/chili.png
    #	app/src/main/res/drawable-xhdpi/chili.png
    #	app/src/main/res/drawable-xxhdpi/chili.png
    #	app/src/main/res/drawable-xxxhdpi/chili.png

--- app/src/main/java/com/example/oderfood/activity/MainActivity.java
+++ app/src/main/java/com/example/oderfood/activity/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.oderfood.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.example.oderfood.R;
-import com.example.oderfood.adapter.CategoryPagerAdapter;
-import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_Layout);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-        }
-
-        viewPager.setAdapter(new CategoryPagerAdapter(getSupportFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
