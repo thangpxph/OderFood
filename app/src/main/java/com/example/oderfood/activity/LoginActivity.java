96461b0263df9e09e4ad697b69c23bd482f681e1
Minh Hai <gitkudo0504@gmail.com>
2020-11-21 01:38:50 +0700

    change UI Login

--- app/src/main/java/com/example/oderfood/activity/LoginActivity.java
+++ app/src/main/java/com/example/oderfood/activity/LoginActivity.java
@@ -5,46 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.oderfood.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText edUsername;
-    private EditText edPassword;
-    private Button btnDangNhap;
-    private TextView tvQuenMatKhau;
-    private TextView tvDangKy;
+    private EditText edtPhone;
+    private EditText edtPassword;
+    private Button btnLogin;
+    private TextView tvRegister;
+    private TextView tvForgotPass;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         setContentView(R.layout.activity_login);
-        edUsername = (EditText) findViewById(R.id.edUsername);
-        edPassword = (EditText) findViewById(R.id.edPassword);
-        btnDangNhap = (Button) findViewById(R.id.btnDangNhap);
-        tvQuenMatKhau = (TextView) findViewById(R.id.tvQuenMatKhau);
-        tvDangKy = (TextView) findViewById(R.id.tvDangKy);
-        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+        edtPhone = (EditText) findViewById(R.id.edt_phone);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        tvRegister = (TextView) findViewById(R.id.tv_register);
+        tvForgotPass = (TextView) findViewById(R.id.tv_forgot_pass);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, BottomNavigation.class));
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,BottomNavigation.class));
+            }
+        });
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));            }
+        });
+        tvForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,ForgotPasswordActivity.class));
+            }
+        });
 
-            }
-        });
-        tvDangKy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-
-            }
-        });
-        tvQuenMatKhau.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
-            }
-        });
     }
+
 }
